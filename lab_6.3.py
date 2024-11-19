36f8721649a032f2cc4e5113d1d93a991a01ea28
drruda <151385965+drruda@users.noreply.github.com>
2024-11-19 16:51:11 +0300

    Delete lab_6.3.py

--- lab_6.3.py
+++
@@ -1,7 +0,0 @@
-text=input("Enter a text: ")
-elements=set(text.lower())
-repeated_elements=set()
-for x in elements:
-    if text.lower().count(x) >= 2:
-        repeated_elements.add(x)
-print(repeated_elements)
